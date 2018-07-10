d07b314aeb932c333d891c853ce884f691af283f
Michelle Kim <pasolre95kim@gmail.com>
2018-07-10 18:19:53 +0000

    automatically backed up by learn

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,10 @@
+require_relative
+'../current_age_for_birth_year.rb'
+
+describe "current_age_for_birth_year method" do
+  it "return the age of a person based on the year of birth" do
+    age_of_person = current_age_for_birth_year(1984)
+
+      expect(age_of_person).to eq(19)
+  end
+end
